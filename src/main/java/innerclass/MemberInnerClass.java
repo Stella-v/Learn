77bd90d39e6d3dbee7a3a3bc410c384abcab4286
fang <1213167718@qq.com>
2021-04-12 17:29:54 +0800

    成员内部类

--- src/main/java/innerclass/MemberInnerClass.java
+++ src/main/java/innerclass/MemberInnerClass.java
@@ -4,23 +4,25 @@ package innerclass;
 public class MemberInnerClass {
 
     public int age = 3;
+
     //提供获取内部类的方法
-    public Member getMember(){
+    public Member getMember() {
         return new Member();
     }
 
-    public void test(){
+    public void test() {
         System.out.println("外部类的test方法");
     }
 
     //成员内部类
     class Member {
         int age = 5;
-        public String beat(){
-            return "心脏在跳动,age=" + MemberInnerClass.this.age ;
+
+        public String beat() {
+            return "心脏在跳动,age=" + MemberInnerClass.this.age;
         }
 
-        public void test(){
+        public void test() {
             System.out.println("内部类的test方法");
         }
 
